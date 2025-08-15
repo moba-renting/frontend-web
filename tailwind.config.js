aa2091c3cfe22b48eb062708290234b498e1878d
JohanHuanca <j.huanca4141@gmail.com>
2025-08-15 12:55:09 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Esto cubre todos los archivos de React en la carpeta src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
